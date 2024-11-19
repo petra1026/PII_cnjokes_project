e5f6e30e3bc71d2cd451da93951e19df0e746d50
petra1026 <149487838+petra1026@users.noreply.github.com>
2024-11-19 11:55:36 +0200

    .

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def index():
     atbilde = requests.get("https://api.chucknorris.io/jokes/random")
     joks = atbilde.json()
-    return render_template("index.html", joks = joks)
+    joks1 = atbilde.text
+    print(type(joks))
+    print("----")
+    print(type(joks1))
+    return render_template("index.html", joks = joks["value"])
 
 if __name__ == "main":
     app.run(port = 5000)
